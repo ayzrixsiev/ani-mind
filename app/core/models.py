72abd7abf55730bb23255be7bd73cb20a75c7bae
Aziz Rixsiev <ayzrixsiev@gmail.com>
2026-01-21 20:17:33 +0500

    refactor: Added auto migration function, improved DB structure by adding relationship

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,27 @@ from sqlalchemy import (
     TIMESTAMP,
 )
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from app.core.database import Base
 from app.core import schemas
+
+
+class User(Base):
+    __tablename__ = "users_table"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False, server_default="user")
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=func.now()
+    )
+    anime_list = relationship(
+        "Anime",  # Target class name (string is fine)
+        back_populates="owner",  # Must match the attribute name we'll add on Anime
+        cascade="all, delete-orphan",  # Delete anime when removed from user list or user deleted
+        passive_deletes=True,  # Helps with ON DELETE CASCADE from DB
+    )
 
 
 class Anime(Base):
@@ -23,15 +42,4 @@ class Anime(Base):
     owner_id = Column(
         Integer, ForeignKey("users_table.id", ondelete="CASCADE"), nullable=False
     )
-
-
-class User(Base):
-    __tablename__ = "users_table"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    role = Column(String, nullable=False, server_default="user")
-    created_at = Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=func.now()
-    )
+    owner = relationship("User", back_populates="anime_list")
